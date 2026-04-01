c66e7b8bf82e62ec818150c8a1bd9b051df185f1
akhila2104 <121490825+akhila2104@users.noreply.github.com>
2026-04-01 11:48:55 +0530

    Update int_database.sql

--- scripts/int_database.sql
+++ scripts/int_database.sql
@@ -75,68 +75,89 @@ CID NVARCHAR(50) ,
 CNTRY NVARCHAR(50)
 )
 
-TRUNCATE TABLE bronze.crm_cust_info
-BULK INSERT bronze.crm_cust_info
-FROM 'C:\Users\AkhilaShaik\OneDrive - STREAMVECTOR TECHNOLOGY PRIVATE LIMITED\Books\Project\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-WITH (
-	FIRSTROW = 2, 
-	FIELDTERMINATOR = ',',
-	TABLOCK 
-)
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	Print '=====================================';
+	Print 'Loading Bronze Layer';
+	Print '=====================================';
 
-SELECT count(*) FROM bronze.crm_prd_info
-----------------------------------------------------------
-TRUNCATE TABLE bronze.crm_prd_info
-BULK INSERT bronze.crm_prd_info
-FROM 'C:\Users\AkhilaShaik\OneDrive - STREAMVECTOR TECHNOLOGY PRIVATE LIMITED\Books\Project\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-WITH (
-	FIRSTROW = 2, 
-	FIELDTERMINATOR = ',',
-	TABLOCK 
-)
+	Print '-------------------------------------';
+	Print 'Loading CRM Tables';
+	Print '-------------------------------------';
 
-SELECT * FROM bronze.crm_prd_info
---------------------------------------------------------------------------
-TRUNCATE TABLE bronze.crm_sales_details
-BULK INSERT bronze.crm_sales_details
-FROM 'C:\Users\AkhilaShaik\OneDrive - STREAMVECTOR TECHNOLOGY PRIVATE LIMITED\Books\Project\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-WITH (
-	FIRSTROW = 2, 
-	FIELDTERMINATOR = ',',
-	TABLOCK 
-)
+	PRINT '>> Truncating Table: bronze.crm_cust_info';
+	TRUNCATE TABLE bronze.crm_cust_info;
 
-SELECT * FROM bronze.crm_sales_details
-------------------------------------------------------------------------------
-TRUNCATE TABLE bronze.erp_cust_az12
-BULK INSERT bronze.erp_cust_az12
-FROM 'C:\Users\AkhilaShaik\OneDrive - STREAMVECTOR TECHNOLOGY PRIVATE LIMITED\Books\Project\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
-WITH (
-	FIRSTROW = 2, 
-	FIELDTERMINATOR = ',',
-	TABLOCK 
-)
+	PRINT '>> Inserting Table: bronze.crm_cust_info';
+	BULK INSERT bronze.crm_cust_info
+	FROM 'C:\Users\AkhilaShaik\OneDrive - STREAMVECTOR TECHNOLOGY PRIVATE LIMITED\Books\Project\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+	WITH (
+		FIRSTROW = 2, 
+		FIELDTERMINATOR = ',',
+		TABLOCK 
+	);
 
-SELECT * FROM bronze.erp_cust_az12
----------------------------------------------------------------------------------
-TRUNCATE TABLE bronze.erp_px_cat_g1v2
-BULK INSERT bronze.erp_px_cat_g1v2
-FROM 'C:\Users\AkhilaShaik\OneDrive - STREAMVECTOR TECHNOLOGY PRIVATE LIMITED\Books\Project\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
-WITH (
-	FIRSTROW = 2, 
-	FIELDTERMINATOR = ',',
-	TABLOCK 
-)
+	PRINT '>> Truncating Table: bronze.crm_prd_info'
+	TRUNCATE TABLE bronze.crm_prd_info;
 
-SELECT * FROM bronze.erp_px_cat_g1v2
---------------------------------------------------------------------------------------
-TRUNCATE TABLE bronze.erp_loc_a101
-BULK INSERT bronze.erp_loc_a101
-FROM 'C:\Users\AkhilaShaik\OneDrive - STREAMVECTOR TECHNOLOGY PRIVATE LIMITED\Books\Project\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
-WITH (
-	FIRSTROW = 2, 
-	FIELDTERMINATOR = ',',
-	TABLOCK 
-)
+	PRINT '>> Inserting Table: bronze.crm_prd_info'
+	BULK INSERT bronze.crm_prd_info
+	FROM 'C:\Users\AkhilaShaik\OneDrive - STREAMVECTOR TECHNOLOGY PRIVATE LIMITED\Books\Project\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+	WITH (
+		FIRSTROW = 2, 
+		FIELDTERMINATOR = ',',
+		TABLOCK 
+	);
 
-SELECT * FROM bronze.erp_loc_a101
+	PRINT '>> Truncating Table: bronze.crm_sales_details';
+	TRUNCATE TABLE bronze.crm_sales_details;
+
+	PRINT '>> Inserting Table: bronze.crm_sales_details';
+	BULK INSERT bronze.crm_sales_details
+	FROM 'C:\Users\AkhilaShaik\OneDrive - STREAMVECTOR TECHNOLOGY PRIVATE LIMITED\Books\Project\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+	WITH (
+		FIRSTROW = 2, 
+		FIELDTERMINATOR = ',',
+		TABLOCK 
+	);
+
+	Print '-------------------------------------';
+	Print 'Loading ERP Tables';
+	Print '-------------------------------------';
+
+	PRINT '>> Truncating Table: bronze.erp_cust_az12';
+	TRUNCATE TABLE bronze.erp_cust_az12;
+
+	PRINT '>> Inserting Table: bronze.erp_cust_az12';
+	BULK INSERT bronze.erp_cust_az12
+	FROM 'C:\Users\AkhilaShaik\OneDrive - STREAMVECTOR TECHNOLOGY PRIVATE LIMITED\Books\Project\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+	WITH (
+		FIRSTROW = 2, 
+		FIELDTERMINATOR = ',',
+		TABLOCK 
+	);
+
+	PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
+	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+	PRINT '>> Inserting Table: bronze.erp_px_cat_g1v2';
+	BULK INSERT bronze.erp_px_cat_g1v2
+	FROM 'C:\Users\AkhilaShaik\OneDrive - STREAMVECTOR TECHNOLOGY PRIVATE LIMITED\Books\Project\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+	WITH (
+		FIRSTROW = 2, 
+		FIELDTERMINATOR = ',',
+		TABLOCK 
+	);
+
+	PRINT '>> Truncating Table: bronze.erp_loc_a101';
+	TRUNCATE TABLE bronze.erp_loc_a101;
+
+	PRINT '>> Inserting Table: bronze.erp_loc_a101';
+	BULK INSERT bronze.erp_loc_a101
+	FROM 'C:\Users\AkhilaShaik\OneDrive - STREAMVECTOR TECHNOLOGY PRIVATE LIMITED\Books\Project\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+	WITH (
+		FIRSTROW = 2, 
+		FIELDTERMINATOR = ',',
+		TABLOCK 
+	);
+END
