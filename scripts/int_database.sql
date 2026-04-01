63adbd1a26c20f18b3ed78a81acf4bfe87a5672b
akhila2104 <121490825+akhila2104@users.noreply.github.com>
2026-04-01 10:48:50 +0530

    Update int_database.sql

--- scripts/int_database.sql
+++ scripts/int_database.sql
@@ -75,4 +75,68 @@ CID NVARCHAR(50) ,
 CNTRY NVARCHAR(50)
 )
 
+TRUNCATE TABLE bronze.crm_cust_info
+BULK INSERT bronze.crm_cust_info
+FROM 'C:\Users\AkhilaShaik\OneDrive - STREAMVECTOR TECHNOLOGY PRIVATE LIMITED\Books\Project\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+WITH (
+	FIRSTROW = 2, 
+	FIELDTERMINATOR = ',',
+	TABLOCK 
+)
 
+SELECT count(*) FROM bronze.crm_prd_info
+----------------------------------------------------------
+TRUNCATE TABLE bronze.crm_prd_info
+BULK INSERT bronze.crm_prd_info
+FROM 'C:\Users\AkhilaShaik\OneDrive - STREAMVECTOR TECHNOLOGY PRIVATE LIMITED\Books\Project\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+WITH (
+	FIRSTROW = 2, 
+	FIELDTERMINATOR = ',',
+	TABLOCK 
+)
+
+SELECT * FROM bronze.crm_prd_info
+--------------------------------------------------------------------------
+TRUNCATE TABLE bronze.crm_sales_details
+BULK INSERT bronze.crm_sales_details
+FROM 'C:\Users\AkhilaShaik\OneDrive - STREAMVECTOR TECHNOLOGY PRIVATE LIMITED\Books\Project\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+WITH (
+	FIRSTROW = 2, 
+	FIELDTERMINATOR = ',',
+	TABLOCK 
+)
+
+SELECT * FROM bronze.crm_sales_details
+------------------------------------------------------------------------------
+TRUNCATE TABLE bronze.erp_cust_az12
+BULK INSERT bronze.erp_cust_az12
+FROM 'C:\Users\AkhilaShaik\OneDrive - STREAMVECTOR TECHNOLOGY PRIVATE LIMITED\Books\Project\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+WITH (
+	FIRSTROW = 2, 
+	FIELDTERMINATOR = ',',
+	TABLOCK 
+)
+
+SELECT * FROM bronze.erp_cust_az12
+---------------------------------------------------------------------------------
+TRUNCATE TABLE bronze.erp_px_cat_g1v2
+BULK INSERT bronze.erp_px_cat_g1v2
+FROM 'C:\Users\AkhilaShaik\OneDrive - STREAMVECTOR TECHNOLOGY PRIVATE LIMITED\Books\Project\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+WITH (
+	FIRSTROW = 2, 
+	FIELDTERMINATOR = ',',
+	TABLOCK 
+)
+
+SELECT * FROM bronze.erp_px_cat_g1v2
+--------------------------------------------------------------------------------------
+TRUNCATE TABLE bronze.erp_loc_a101
+BULK INSERT bronze.erp_loc_a101
+FROM 'C:\Users\AkhilaShaik\OneDrive - STREAMVECTOR TECHNOLOGY PRIVATE LIMITED\Books\Project\dbc9660c89a3480fa5eb9bae464d6c07\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+WITH (
+	FIRSTROW = 2, 
+	FIELDTERMINATOR = ',',
+	TABLOCK 
+)
+
+SELECT * FROM bronze.erp_loc_a101
